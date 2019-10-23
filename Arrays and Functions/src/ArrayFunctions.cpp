e2f526644add3d3294027902ac0c2c3c81ba0db3
nrLucas <56032832+nrLucas@users.noreply.github.com>
2019-10-23 16:33:05 -0300

    Count minimum steps to get the given desired array
    Consider an array with n elements and value of all the elements is zero. We can perform following operations on the array.
    
    Incremental operations:Choose 1 element from the array and increment its value by 1.
    Doubling operation: Double the values of all the elements of array.

--- Arrays and Functions/src/ArrayFunctions.cpp
+++ Arrays and Functions/src/ArrayFunctions.cpp
@@ -1,58 +1,71 @@
-// O(n) solution for finding smallest subarray with sum 
-// greater than x 
-#include <iostream> 
+/* C++ program to count minimum number of operations 
+   to get the given target array */
+#include <bits/stdc++.h> 
 using namespace std; 
   
-// Returns length of smallest subarray with sum greater than x. 
-// If there is no subarray with given sum, then returns n+1 
-int smallestSubWithSum(int arr[], int n, int x) 
+// Returns count of minimum operations to covert a 
+// zero array to target array with increment and 
+// doubling operations. 
+// This function computes count by doing reverse 
+// steps, i.e., convert target to zero array. 
+int countMinOperations(unsigned int target[], int n) 
 { 
-    // Initialize current sum and minimum length 
-    int curr_sum = 0, min_len = n+1; 
+    // Initialize result (Count of minimum moves) 
+    int result = 0; 
   
-    // Initialize starting and ending indexes 
-    int start = 0, end = 0; 
-    while (end < n) 
+    // Keep looping while all elements of target 
+    // don't become 0. 
+    while (1) 
     { 
-        // Keep adding array elements while current sum 
-        // is smaller than x 
-        while (curr_sum <= x && end < n) 
-        { 
-            // Ignore subarrays with negative sum if 
-            // x is positive. 
-            if (curr_sum <= 0 && x > 0) 
-            { 
-                start = end; 
-                curr_sum = 0; 
-            } 
+        // To store count of zeroes in current 
+        // target array 
+        int zero_count = 0; 
   
-            curr_sum += arr[end++]; 
+        int i;  // To find first odd element 
+        for (i=0; i<n; i++) 
+        { 
+            // If odd number found 
+            if (target[i] & 1) 
+                break; 
+  
+            // If 0, then increment zero_count 
+            else if (target[i] == 0) 
+                zero_count++; 
         } 
   
-        // If current sum becomes greater than x. 
-        while (curr_sum > x && start < n) 
-        { 
-            // Update minimum length if needed 
-            if (end - start < min_len) 
-                min_len = end - start; 
+        // All numbers are 0 
+        if (zero_count == n) 
+          return result; 
   
-            // remove starting elements 
-            curr_sum -= arr[start++]; 
+        // All numbers are even 
+        if (i == n) 
+        { 
+            // Divide the whole array by 2 
+            // and increment result 
+            for (int j=0; j<n; j++) 
+               target[j] = target[j]/2; 
+            result++; 
+        } 
+         // Make all odd numbers even by subtracting 
+        // one and increment result. 
+        for (int j=i; j<n; j++) 
+        { 
+           if (target[j] & 1) 
+           { 
+              target[j]--; 
+              result++; 
+           } 
         } 
     } 
-    return min_len; 
 } 
   
-  
-/* Driver program to test above function */
+/* Driver program to test above functions*/
 int main() 
 { 
-    int arr1[] = {- 8, 1, 4, 2, -6}; 
-    int x = 6; 
-    int n1 = sizeof(arr1)/sizeof(arr1[0]); 
-    int res1 = smallestSubWithSum(arr1, n1, x); 
-    (res1 == n1+1)? cout << "Not possible\n" : 
-                    cout << res1 << endl; 
-  
+    unsigned int arr[] = {16, 16, 16}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    cout << "Minimum number of steps required to "
+           "get the given target array is " 
+         << countMinOperations(arr, n); 
     return 0; 
- }
+} 
